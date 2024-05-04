67d870e64bfd6850f2b5fc16e67a6591723e796e
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-05-04 16:26:56 -0700

    initial working dwl flake install

--- flake.nix
+++ flake.nix
@@ -7,25 +7,40 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    dwl-flake = {
+      url = "github:maxbfuer/dwl";
+      inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
+    dwl-flake,
     ...
   } @ inputs: {
-    nixosConfigurations.gaia = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.gaia = let
       system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.max = import ./home.nix;
-        }
-      ];
-    };
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.max = import ./home.nix;
+          }
+          {
+            environment.systemPackages = [
+              dwl-flake.packages.x86_64-linux.dwl
+            ];
+          }
+        ];
+      };
   };
 }
