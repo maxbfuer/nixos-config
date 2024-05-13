a2bd8df72e8d1fa85a72a4d29e8024b0dfcfea8c
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-05-13 14:30:38 -0700

    add some git aliases

--- home/git.nix
+++ home/git.nix
@@ -1,8 +1,20 @@
 {...}: {
   programs.git = {
     enable = true;
+
     userName = "Maxwell Fuerstenberg";
     userEmail = "max@maxfuerstenberg.com";
+
+    aliases = {
+      st = "status -sb";
+      last = "log -1 HEAD --stat";
+      cm = "commit -m";
+      d = "diff";
+      dc = "diff --cached";
+      l = "log --pretty=format:\"%C(yellow)%h\\ %C(green)%ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --date=short --graph";
+      a = "add";
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
     };
