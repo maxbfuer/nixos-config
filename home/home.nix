9defe91abf1977ede869ce3e7e71df56b9bade2a
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-05-14 14:58:09 -0700

    install and configure tofi as the launcher for dwl

--- home/home.nix
+++ home/home.nix
@@ -20,11 +20,29 @@
     blueman # bluetooth device manager GUI
     pavucontrol # audio control GUI
     obsidian # markdown knowledge base
-
-    # -- dwl bindings --
-    kickoff # fast and responsive dmenu replacement
-    fuzzel # .desktop entry launcher
   ];
+
+  # dwl binding: very fast, simple dmenu/rofi for wlroots
+  programs.tofi = {
+    enable = true;
+    settings = {
+      # specifying the font file improves startup time
+      # see: https://github.com/philj56/tofi?tab=readme-ov-file#performance
+      font = "${pkgs.monaspace}/share/fonts/opentype/MonaspaceNeon-Regular.otf";
+      fuzzy-match = true;
+      prompt-text = "\"> \"";
+      # "fullscreen" theme
+      width = "100%";
+      height = "100%";
+      border-width = 0;
+      outline-width = 0;
+      padding-left = "40%";
+      padding-top = "10%";
+      result-spacing = 25;
+      num-results = 15;
+      background-color = "#000A";
+    };
+  };
 
   programs.vscode = {
     enable = true;
