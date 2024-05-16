0b881f9921ea28176f60534407248774c485bd3a
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-05-16 01:23:30 -0700

    fix: correctly use dash instead of bash in scripts
    I was using writeShellScriptBin, apparently it (and writeShellScript)
    insert a bash shebang at the top of written scripts, overriding my dash
    shebang. writeScriptBin does no such thing, and allows my dash
    shebangs to work correctly.
    
    Personally I think "writeBashScriptBin" would have been a better name.

--- home/scripts.nix
+++ home/scripts.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: let
   scriptDir = ./scripts;
   scriptFiles = builtins.attrNames (builtins.readDir scriptDir);
-  scriptPackages = map (file: pkgs.writeShellScriptBin file (builtins.readFile "${scriptDir}/${file}")) scriptFiles;
+  scriptPackages = map (file: pkgs.writeScriptBin file (builtins.readFile "${scriptDir}/${file}")) scriptFiles;
 in {
   home.packages = scriptPackages;
 }
