12776e8a0c5f72d37aafdf1e41ca8e8cc6708fd7
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-05-24 16:04:27 -0700

    increase root tmpfs to 75% of my RAM

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -20,7 +20,7 @@
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
-    options = ["defaults" "size=4G" "mode=755"];
+    options = ["defaults" "size=75%" "mode=755"];
   };
 
   fileSystems."/boot" = {
