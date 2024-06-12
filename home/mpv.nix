08fa6ff32dd668c0c3871eb2d356ba91bd722f12
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-06-11 20:30:27 -0700

    mpv config: enable hwdec, select english audio/sub lang
    I've temporarily disabled the playlistmanager plugin, there seems to be
    a bug in this version of the package that is preventing it from installing.

--- home/mpv.nix
+++ home/mpv.nix
@@ -3,14 +3,19 @@
     enable = true;
     config = {
       profile = "gpu-hq";
+      hwdec = "auto";
       force-window = true;
+      save-position-on-quit = true;
       cache = true;
       demuxer-max-bytes = "512MiB";
       demuxer-max-back-bytes = "256MiB";
-      save-position-on-quit = true;
+      # select english audio and subs, but with subs hidden by default
+      alang = "eng,en";
+      slang = "eng,en";
+      sub-visibility = false;
     };
-    scripts = with pkgs.mpvScripts; [
-      mpv-playlistmanager
-    ];
+    # scripts = with pkgs.mpvScripts; [
+    #   mpv-playlistmanager
+    # ];
   };
 }
