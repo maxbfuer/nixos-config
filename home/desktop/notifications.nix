013598cf158de6a161a2500050894afce1688065
Maxwell Fuerstenberg <max@maxfuerstenberg.com>
2024-06-15 15:40:24 -0700

    configure fnott

--- home/desktop/notifications.nix
+++ home/desktop/notifications.nix
@@ -6,5 +6,19 @@
   # lightweight wlroots notification daemon
   services.fnott = {
     enable = true;
+    settings = {
+      main = {
+        output = "DP-1";
+        selection-helper = "tofi";
+        min-width = 160;
+        padding-vertical = 10;
+        padding-horizontal = 10;
+        default-timeout = 10;
+        max-timeout = 20;
+        # notifications will not time out if I am idle
+        idle-timeout = 5;
+        # border-color?
+      };
+    };
   };
 }
